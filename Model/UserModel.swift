5c80b0bf6a6ddfc88bb1eef07d720e685d19ecd0
Rayhan Faluda <rayhanfaluda@gmail.com>
2021-12-13 20:45:37 +0700

    Add Model and ViewModel
    - Add UserModel
    - Add ProfileViewModel

--- Model/UserModel.swift
+++ Model/UserModel.swift
@@ -7,20 +7,20 @@
 
 import Foundation
 
-struct User: Identifiable, Codable {
-    let id: Int
-    let fullname: String
-    let password: String
-    let age: Int?
-    let email: String
-    let gender: String
-    let birthDate: Date?
-    let phoneNumber: String?
-    let imagePath: String?
-    let isAdmin: Bool?
-    let isActive: Bool?
-    let createdAt: Date?
-    let updatedAt: Date?
+struct UserModel: Identifiable, Codable {
+    var id: Int?
+    var fullname: String?
+    var password: String?
+    var age: Int?
+    var email: String?
+    var gender: String?
+    var birthDate: Date?
+    var phoneNumber: String?
+    var imagePath: String?
+    var isAdmin: Bool?
+    var isActive: Bool?
+    var createdAt: Date?
+    var updatedAt: Date?
     
     enum CodingKeys: String, CodingKey {
         case id
@@ -36,5 +36,22 @@ struct User: Identifiable, Codable {
         case isActive = "is_active"
         case createdAt = "created_at"
         case updatedAt = "updated_at"
+    }
+    
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        id = try values.decodeIfPresent(Int.self, forKey: .id)
+        fullname = try values.decodeIfPresent(String.self, forKey: .fullname)
+        password = try values.decodeIfPresent(String.self, forKey: .password)
+        age = try values.decodeIfPresent(Int.self, forKey: .age)
+        email = try values.decodeIfPresent(String.self, forKey: .email)
+        gender = try values.decodeIfPresent(String.self, forKey: .gender)
+        birthDate = try values.decodeIfPresent(Date.self, forKey: .birthDate)
+        phoneNumber = try values.decodeIfPresent(String.self, forKey: .phoneNumber)
+        imagePath = try values.decodeIfPresent(String.self, forKey: .imagePath)
+        isAdmin = try values.decodeIfPresent(Bool.self, forKey: .isAdmin)
+        isActive = try values.decodeIfPresent(Bool.self, forKey: .isActive)
+        createdAt = try values.decodeIfPresent(Date.self, forKey: .createdAt)
+        updatedAt = try values.decodeIfPresent(Date.self, forKey: .updatedAt)
     }
 }
